be54902ca3be6de46f6c5fca227b086a9f152af4
Suzie K <suziekim.dev@gmail.com>
2021-10-14 16:35:17 -0400

    [Test] Red lists 151 pokemon in alphabetical order

--- spec/features/pokemon/index_spec.rb
+++ spec/features/pokemon/index_spec.rb
@@ -11,5 +11,17 @@ RSpec.describe "Pokemon Index page" do
       expect(page).to have_content("Pokemon - Generation 1")
       expect('bulbasaur').to appear_before('mew')
     end
+
+    context 'user can clicks order alphabetically button' do 
+      it 'lists 151 Pokemon in alphabetical order' do 
+        expect('bulbasaur').to appear_before('abra')
+        expect('zubat').to appear_before('mew')
+        
+        click_link "Sort Alphabetically"
+       
+        expect('abra').to appear_before('bulbasaur')
+        expect('mew').to appear_before('zubat')
+      end
+    end
   end
 end 
